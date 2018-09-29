151d61381cf40707cce354fead2d93f20a22b9da
Michael Ebenstein <mebenstein@student.tgm.ac.at>
2018-09-29 18:13:23 +0200

    added stuff

---
+++ cmake-test/CMakeFiles/cmake-test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-test.dir/test.cpp.obj"
+  "cmake-test.pdb"
+  "cmake-test.exe"
+  "cmake-test.exe.manifest"
+  "cmake-test.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
